a0d4258aeae9e4ec8871b7cf5e9ded04cf5b70d0
Haqqi <me@haqqi.net>
2016-11-21 15:56:04 +0700

    Finish basic bootstrap panel

--- src/views/feature/panel.php
+++ src/views/feature/panel.php
@@ -2,9 +2,48 @@
 
 ?>
 
+<div class="row">
+  <div class="col-sm-6">
+    <section class="panel panel-default">
+      <header class="panel-heading">
+        <h3 class="panel-title"><i class="fa fa-clock-o"></i> Simple Panel</h3>
+        <span class="panel-description">Put your content below.</span>
+      </header>
+      <div class="panel-body">
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fer</p>
+      </div>
+      <footer class="panel-footer">
+        Panel Footer
+      </footer>
+    </section>
+  </div>
+  <div class="col-sm-6">
+    <section class="panel panel-primary">
+      <header class="panel-heading">
+        <h3 class="panel-title"><i class="fa fa-clock-o"></i> Panel with column</h3>
+        <span class="panel-description">Put your content below.</span>
+      </header>
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-md-6"><p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio
+              sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi
+              erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras
+              mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor
+              ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. consectetur
+              purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia
+              odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. nisi erat porttitor ligula, eget
+              lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo
+              luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+              fermentum. consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor
+              ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fer</p></div>
+          <div class="col-md-6"><p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fer</p>
+          </div>
+        </div>
 
-<h2>Standard Bootstrap Grid</h2>
-
-
+      </div>
+    </section>
+  </div>
+</div>
 
 <?php
