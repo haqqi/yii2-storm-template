b2b459d6bdae1034b0b2f82b7e7f65507fb5f633
Haqqi <me@haqqi.net>
2016-11-23 12:02:21 +0700

    Add content negotiator for ajax

--- src/controllers/StormController.php
+++ src/controllers/StormController.php
@@ -3,6 +3,7 @@
 namespace haqqi\storm\controllers;
 
 use haqqi\storm\Storm;
+use yii\filters\ContentNegotiator;
 use yii\web\Controller;
 
 /**
@@ -18,5 +19,20 @@ class StormController extends Controller {
     parent::init();
 
     $this->layout = Storm::getComponent()->getLayoutPath();
+  }
+
+  public function behaviors() {
+    $behaviors = parent::behaviors();
+
+    // make the response json according to the request
+    $behaviors['contentNegotiator'] = [
+      'class'   => ContentNegotiator::className(),
+      'formats' => [
+        'application/json' => Response::FORMAT_JSON,
+        'text/html'        => Response::FORMAT_HTML
+      ],
+    ];
+
+    return $behaviors;
   }
 }
