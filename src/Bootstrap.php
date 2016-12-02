65b09d15d60c8dddd52dd7b9c057d734341b5099
Muhammad Fauzil Haqqi <me@haqqi.net>
2016-12-02 22:57:57 +0700

    Fix bootstraping in console controller

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -20,19 +20,21 @@ class Bootstrap implements BootstrapInterface {
       /*
        * Setup the config of asset bundles
        */
-      $bundles =& $event->sender->assetManager->bundles;
+      if(!$event->action->controller instanceof Controller) {
+        $bundles =& $event->sender->assetManager->bundles;
 
-      $bundles['yii\web\JqueryAsset']['js']        = ['jquery.min.js'];
-      $bundles['yii\web\JqueryAsset']['jsOptions'] = ['position' => View::POS_HEAD];
+        $bundles['yii\web\JqueryAsset']['js']        = ['jquery.min.js'];
+        $bundles['yii\web\JqueryAsset']['jsOptions'] = ['position' => View::POS_HEAD];
 
-      $bundles['yii\bootstrap\BootstrapAsset']['css'] = ['css/bootstrap.min.css'];
+        $bundles['yii\bootstrap\BootstrapAsset']['css'] = ['css/bootstrap.min.css'];
 
-      $bundles['yii\bootstrap\BootstrapPluginAsset']['js']        = ['js/bootstrap.min.js'];
-      $bundles['yii\bootstrap\BootstrapPluginAsset']['jsOptions'] = ['position' => \yii\web\View::POS_HEAD];
+        $bundles['yii\bootstrap\BootstrapPluginAsset']['js']        = ['js/bootstrap.min.js'];
+        $bundles['yii\bootstrap\BootstrapPluginAsset']['jsOptions'] = ['position' => View::POS_HEAD];
 
-      $bundles['mimicreative\assets\MetisMenuAsset']['css'] = [];
+        $bundles['mimicreative\assets\MetisMenuAsset']['css'] = [];
 
-      \FB::log($event->sender->assetManager->bundles);
+//        \FB::log($event->sender->assetManager->bundles);
+      }
     });
   }
 
