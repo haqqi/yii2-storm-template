3d7e96b9065199457bd82f997f8dd06922cca146
Muhammad Fauzil Haqqi <me@haqqi.net>
2016-12-07 12:09:53 +0700

    Start developing datatables extension

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,11 +2,9 @@
 
 namespace haqqi\storm;
 
-
 use yii\base\Application;
 use yii\base\BootstrapInterface;
 use yii\base\Event;
-use yii\console\Controller;
 use yii\web\View;
 
 class Bootstrap implements BootstrapInterface {
@@ -18,24 +16,23 @@ class Bootstrap implements BootstrapInterface {
        * @var $event Event
        */
 
-    \Yii::$container->set('yii\web\JqueryAsset', [
-      'js' => ['jquery.min.js'],
-      'jsOptions' => ['position' => View::POS_HEAD]
-    ]);
+      \Yii::$container->set('yii\web\JqueryAsset', [
+        'js'        => ['jquery.min.js'],
+        'jsOptions' => ['position' => View::POS_HEAD]
+      ]);
 
-    \Yii::$container->set('yii\bootstrap\BootstrapAsset', [
-      'css' => ['css/bootstrap.min.css']
-    ]);
+      \Yii::$container->set('yii\bootstrap\BootstrapAsset', [
+        'css' => ['css/bootstrap.min.css']
+      ]);
 
-    \Yii::$container->set('yii\bootstrap\BootstrapPluginAsset', [
-      'js' => ['js/bootstrap.min.js'],
-      'jsOptions' => ['position' => View::POS_HEAD]
-    ]);
+      \Yii::$container->set('yii\bootstrap\BootstrapPluginAsset', [
+        'js'        => ['js/bootstrap.min.js'],
+        'jsOptions' => ['position' => View::POS_HEAD]
+      ]);
 
-    \Yii::$container->set('mimicreative\assets\MetisMenuAsset', [
-      'css' => []
-    ]);
-
+      \Yii::$container->set('mimicreative\assets\MetisMenuAsset', [
+        'css' => []
+      ]);
     });
   }
 
